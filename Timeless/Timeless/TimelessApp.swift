83b7c2a11ca60588a75acddf3052483940a88efa
Phuc <ngocphuc00001@gmail.com>
2023-05-12 13:21:28 +0700

    update

---
+++ Timeless/Timeless/TimelessApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelessApp.swift
+//  Timeless
+//
+//  Created by Zien on 12/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TimelessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
